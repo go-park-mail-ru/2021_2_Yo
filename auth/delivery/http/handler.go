04b9043e393a715b39621751cae8e70496d36b3f
Shirshov Artyom <artyomsh01@yandex.ru>
2021-09-26 02:54:30 +0300

    again

--- auth/delivery/http/handler.go
+++ auth/delivery/http/handler.go
@@ -93,6 +93,7 @@ func (h *HandlerAuth) Auth(w http.ResponseWriter, r *http.Request) {
 	log.Println("In auth")
 	defer r.Body.Close()
 	kukan, err := r.Cookie("auth")
+	log.Println(kukan)
 	if err != nil {
 		log.Println("in error")
 		cookies["rarara"] = "Blabla"
