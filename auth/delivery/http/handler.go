b9d5a54e8b3365df7b3d0c9b76fce9cbe29a52bb
Shirshov Artyom <artyomsh01@yandex.ru>
2021-09-26 02:39:27 +0300

    again

--- auth/delivery/http/handler.go
+++ auth/delivery/http/handler.go
@@ -85,7 +85,7 @@ func (h *HandlerAuth) Test(w http.ResponseWriter, r *http.Request) {
 
 func (h *HandlerAuth) Auth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	cookies := make(map[string]string)
 	log.Println("In auth")
