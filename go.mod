5a5f70a6f7857c6f52408fcbe6bb2abd909d10d8
Shirshov Artyom <artyomsh01@yandex.ru>
2021-09-26 01:05:29 +0300

    smth

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module backend
 go 1.13
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0
-	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/rs/cors v1.8.0
+	github.com/sirupsen/logrus v1.8.1
 )
--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-
+	"github.com/rs/cors"
+	//"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
@@ -17,7 +18,6 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	fmt.Fprintln(w, "Главная страница")
-	http.Redirect(w,r,"http://bmstusa.herokuapp.com/",http.StatusSeeOther)
 }
 
 func main() {
@@ -35,16 +35,23 @@ func main() {
 	handler := deliveryAuth.NewHandlerAuth(useCase)
 
 	r.HandleFunc("/", mainPage)
-	r.HandleFunc("/signup", handler.SignUp).Methods("GET","POST")
+	r.HandleFunc("/signup", handler.SignUp).Methods("POST")
 	r.HandleFunc("/signin", handler.SignIn).Methods("POST")
 	//Нужен метод для SignIn с методом GET
 
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://bmstusa.herokuapp.com/"},
+		AllowCredentials: true,
+
+	})
+
+	mainHandler := c.Handler(r)
+
 	log.Info("Deploying. Port: ", port)
-	err := http.ListenAndServe(":"+port, r)
+	err := http.ListenAndServe(":"+port, mainHandler)
 	if err != nil {
 		log.Error("main error: ", err)
 	}
-
 
 
 }
