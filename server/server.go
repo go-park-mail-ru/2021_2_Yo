1d3e1af873dbec4e0381347b447ddc05f45263e7
comradyo <acesolo@mail.ru>
2021-11-01 02:27:08 +0300

    err

--- server/server.go
+++ server/server.go
@@ -97,9 +97,9 @@ func newRouterWithEndpoints(app *App) *mux.Router {
 	r.Handle("/user/password", authRouter)
 	r.HandleFunc("/events", app.eventManager.List).Methods("GET")
 	r.HandleFunc("/events/{id:[0-9]+}", app.eventManager.GetEvent).Methods("GET")
-	//r.Handle("/events/{id:[0-9]+}", authRouter)
-	r.HandleFunc("/events/{id:[0-9]+}", app.eventManager.UpdateEvent).Methods("POST")
-	r.HandleFunc("/events/{id:[0-9]+}", app.eventManager.DeleteEvent).Methods("DELETE")
+	r.Handle("/events/{id:[0-9]+}", authRouter)
+	//r.HandleFunc("/events/{id:[0-9]+}", app.eventManager.UpdateEvent).Methods("POST")
+	//r.HandleFunc("/events/{id:[0-9]+}", app.eventManager.DeleteEvent).Methods("DELETE")
 	r.Handle("/events", authRouter)
 	r.PathPrefix("/documentation").Handler(httpSwagger.WrapHandler)
 
