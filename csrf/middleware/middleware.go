427154f6843a30db236f47928226938256e93406
Artyom Shirshov <artyomsh01@yandex.ru>
2021-11-02 14:22:13 +0300

    Merged with img

--- csrf/middleware/middleware.go
+++ csrf/middleware/middleware.go
@@ -23,7 +23,7 @@ const logMessage = "session:middleware:"
 func (m *Middleware) CSRF(next http.Handler) http.Handler {
 	message := logMessage + "CSRF:"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		gottenToken := w.Header().Get("X-CSRF-Token")
+		gottenToken := (*r).Header.Get("X-CSRF-Token")
 		log.Info("gottenToken", gottenToken)
 		userId, err := m.manager.Check(gottenToken)
 		if !utils.CheckIfNoError(&w, err, message, http.StatusNotFound) {
