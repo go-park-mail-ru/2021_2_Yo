b40c11ea7c7ee78a8f5aac568ed3a3bc339adea3
comradyo <acesolo@mail.ru>
2021-11-09 22:08:53 +0300

    session debug

--- service/session/repository/redis.go
+++ service/session/repository/redis.go
@@ -37,8 +37,9 @@ func (s *Repository) Check(sessionId string) (string, error) {
 }
 
 func (s *Repository) Delete(sessionId string) error {
+	log.Debug("Redis delete sessionId =", sessionId)
 	result, err := redis.String(s.db.Do("DEL", sessionId))
-	log.Debug("Redis delete err =", err)
+	log.Error("Redis delete err =", err)
 	log.Debug("Redis delete result = ", result)
 	if err != nil {
 		return error2.ErrDeleteSession
