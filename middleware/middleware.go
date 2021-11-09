9296ed3a92bbd8fb54cc87140f4e2b85f88c4f7a
comradyo <acesolo@mail.ru>
2021-11-09 19:20:06 +0300

    url for cors

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -39,7 +39,7 @@ func (m *Middlewares) Recovery(next http.Handler) http.Handler {
 
 func (m *Middlewares) CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://bmstusssa.herokuapp.com")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers",
 			"Accept,Content-Type,Content-Length,Accept-Encoding,X-CSRF-Token,Authorization")
