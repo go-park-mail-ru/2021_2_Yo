9de24b1a9d1d65255682eacf442a61b324f29d5f
Artyom Shirshov <artyomsh01@yandex.ru>
2021-11-29 19:47:14 +0300

    Test images + hosts

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -40,9 +40,9 @@ func (m *Middlewares) Recovery(next http.Handler) http.Handler {
 
 func (m *Middlewares) CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		mainHost := viper.GetString("main_host")
-		debugHost := viper.GetString("debug_host")
-		w.Header().Set("Access-Control-Allow-Origin", mainHost + "," + debugHost)
+		//mainHost := viper.GetString("main_host")
+		//debugHost := viper.GetString("debug_host")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers",
 			"Accept,Content-Type,Content-Length,Accept-Encoding,X-CSRF-Token,Authorization")
