3e9ca7150796b3131c857560b92e0756c5043ec2
comradyo <acesolo@mail.ru>
2021-12-16 03:18:07 +0300

    auth debug

--- pkg/notificator/notificator.go
+++ pkg/notificator/notificator.go
@@ -70,6 +70,7 @@ func (n *Notificator) DeleteSubscribeNotification(receiverId string, userId stri
 }
 
 func (n *Notificator) InvitationNotification(receiverId string, userId string, eventId string) error {
+	log.Debug("InviteNotification Started")
 	u, err := n.uRepository.GetUserById(userId)
 	if err != nil {
 		return err
@@ -105,6 +106,7 @@ func (n *Notificator) InvitationNotification(receiverId string, userId string, e
 }
 
 func (n *Notificator) NewEventNotification(receiverId string, userId string, eventId string) error {
+	log.Debug("NewEventNotification Started")
 	u, err := n.uRepository.GetUserById(userId)
 	if err != nil {
 		return err
