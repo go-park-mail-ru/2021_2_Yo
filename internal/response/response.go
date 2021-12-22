6e09d5ef201e99691e4d11e7161dd23c58f15239
comradyo <acesolo@mail.ru>
2021-12-22 15:49:23 +0300

    validate fix

--- internal/response/response.go
+++ internal/response/response.go
@@ -35,17 +35,17 @@ type EventIDResponseBody struct {
 
 type EventResponseBody struct {
 	ID          string   `json:"id,omitempty"`
-	Title       string   `json:"title" valid:"type(string),length(0|255)" san:"xss"`
-	Description string   `json:"description" valid:"type(string),length(0|500)" san:"xss"`
-	Text        string   `json:"text" valid:"type(string),length(0|2200)" san:"xss"`
-	City        string   `json:"city" valid:"type(string),length(0|30)" san:"xss"`
+	Title       string   `json:"title" valid:"type(string),length(0|520)" san:"xss"`
+	Description string   `json:"description" valid:"type(string),length(0|1020)" san:"xss"`
+	Text        string   `json:"text" valid:"type(string),length(0|5000)" san:"xss"`
+	City        string   `json:"city" valid:"type(string),length(0|60)" san:"xss"`
 	Category    string   `json:"category" valid:"type(string),length(0|30)" san:"xss"`
 	Viewed      int      `json:"viewed" valid:"type(int)" san:"xss"`
 	ImgUrl      string   `json:"imgUrl" valid:"type(string),length(0|255)" san:"xss"`
 	Tag         []string `json:"tag" san:"xss"`
 	Date        string   `json:"date" valid:"type(string),length(0|10)" san:"xss"`
 	Geo         string   `json:"geo" valid:"type(string),length(0|255)"`
-	Address     string   `json:"address" valid:"type(string), length(0|255)" san:"xss"`
+	Address     string   `json:"address" valid:"type(string), length(0|520)" san:"xss"`
 	AuthorID    string   `json:"authorid" san:"xss"`
 	IsVisited   bool     `json:"favourite"`
 }
